835b8356b5099cd47dfe8e23be03775f2e6c661a
RamadhanIY <ihsani.yulfa@gmail.com>
2023-08-09 15:40:23 +0700

    navbar_bottom
    floating button masih gabisa ditengah wkwk

---
+++ app/src/main/java/com/example/myapplication/User/HomepageUser.java
@@ -0,0 +1,21 @@
+package com.example.myapplication.User;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class HomepageUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_user_homepage);
+    }
+}
